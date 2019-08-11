ce3ac19b36d958c887bfdfed19e23155554d7796
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-08-10 23:28:05 -0300

    change 1 (random numbers)

--- src/com/iain/arcwood/Broadcast.java
+++ src/com/iain/arcwood/Broadcast.java
@@ -5,12 +5,13 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class Broadcast {
 
-	//add &#(number) to add colour, regex will fix
-	private String[] msg = { "xd", "&1hello, World!", "This is a test" };
-	
+	// add &#(number) to add colour, regex will fix
+	private String[] messages = { "xd", "hello, World!", "This is a test" };
+	private String[] textColours = { "&b", "&4", "&c", "&6", "&e", "&a", "&9", "&d" };
+
 	public Broadcast(Main main) {
 
-		this.msg[0] = "lol this is an xd";
+		this.messages[0] = "lol this is an xd";
 		sendBroadcast(main);
 
 	}
@@ -18,14 +19,28 @@ public class Broadcast {
 	public void sendBroadcast(Main main) {
 
 		BukkitRunnable runnable = new BukkitRunnable() {
+			
+			String lastmsg = null;
+			int randomIndex;
+			int randomColour;
 
 			@Override
 			public void run() {
-				Bukkit.broadcastMessage(msg[1].replaceAll("(?<!\\\\)&(?=[0-9A-fK-o])", "§"));
+				randomIndex = (int) (Math.random() * (messages.length - 1));
+				randomColour = (int) (Math.random() * (textColours.length - 1));
+				if(lastmsg == null) {
+					lastmsg = messages[randomIndex];
+				}else {
+					while(lastmsg == messages[randomIndex]) {
+						randomIndex = (int) (Math.random() * (messages.length - 1));
+					}
+				}
+				
+				Bukkit.broadcastMessage((textColours[randomColour] + messages[randomIndex]).replaceAll("(?<!\\\\)&(?=[0-9A-fK-o])", "§"));
 			}
 
 		};
-		//main, start delay, delay between running 20: 20 ticks : 1 second real time.
+		// main, start delay, delay between running 20: 20 ticks : 1 second real time.
 		runnable.runTaskTimer(main, 0, 20 * 60);
 
 	}
